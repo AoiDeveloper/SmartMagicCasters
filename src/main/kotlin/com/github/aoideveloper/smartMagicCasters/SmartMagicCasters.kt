a91e4bc10e27b0b5d6340e33609aafecb53267a6
MakiZushi <aoibotowner@gmail.com>
2025-05-05 16:13:23 +0900

    initial commit

---
+++ src/main/kotlin/com/github/aoideveloper/smartMagicCasters/SmartMagicCasters.kt
@@ -0,0 +1,14 @@
+package com.github.aoideveloper.smartMagicCasters
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class SmartMagicCasters : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
